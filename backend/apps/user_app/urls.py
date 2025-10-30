36c1e825ecd96cdb468c5d90d50ab613d474cd6a
Rumeysa Akgün İleri <rumeysaakgunileri@192.168.1.10>
2025-10-30 23:26:27 +0300

    yorum satırları kaldırıldı

--- backend/apps/user_app/urls.py
+++ backend/apps/user_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import UserSignupView, UserProfileView
 
 urlpatterns = [
-    # sign-up-screen.tsx'ten gelen POST isteği için
+    
     path('auth/signup/', UserSignupView.as_view(), name='user_signup'), 
-    # user-profile.tsx'e veri sağlamak için
+   
     path('profile/me/', UserProfileView.as_view(), name='user_profile'),
 ]
