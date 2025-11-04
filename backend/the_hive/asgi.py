d34a0ebacb3fd3b18c2dd8660cbfb607dfcdd8ec
Rumeysa Akgün İleri <rumeysaakgunileri@192.168.1.10>
2025-11-04 19:55:27 +0300

    frontend setup

--- backend/the_hive/asgi.py
+++ backend/the_hive/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SWE573_term_project project.
+ASGI config for the_hive project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SWE573_term_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_hive.settings")
 
 application = get_asgi_application()
